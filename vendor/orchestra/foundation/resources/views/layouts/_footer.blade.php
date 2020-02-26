706e1e08b33ff8806d43a604730a7f086bcd691e
sudhi seshachala <sudhi@b2bsphere.com>
2020-02-26 20:32:50 +0000

    minor code changes

--- vendor/orchestra/foundation/resources/views/layouts/_footer.blade.php
+++ vendor/orchestra/foundation/resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="row">
     <div class="columns twelve-xs">
-      Powered by <a href="http://orchestraplatform.com" target="_blank">Orchestra Platform</a>.
+      Powered by <a href="http://mangal.hpe.com" target="_blank">Mangal Platform</a>.
     </div>
   </div>
 </footer>
